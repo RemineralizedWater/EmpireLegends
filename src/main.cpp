bce8d5942eee875725399ffa4f7d8e6ece5795e7
whuras <46137966+whuras@users.noreply.github.com>
2021-02-16 18:26:28 -0500

    Implementation of Part 5 - Bidding Facility (#2)
    * Implementation of Part 5 - Bidding Facility
    
    * Added iostream operators, constructors, and assignment operators
    
    * Added missed assignment operator
    
    * Updated data members to be of pointer type, as required in outline.
    
    * Updated numberOfPlayers to be requested in main().

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
 void DemonstratePlayers();
+
+void DemonstrateBiddingFacility(int numberOfPlayers);
+
 int main() {
+    int numberOfPlayers = 0;
+
+    // Determine the number of players
+    cout << "Enter the number of players:";
+    while(true){
+        if(cin >> numberOfPlayers && numberOfPlayers >= 2 && numberOfPlayers <= 4){
+            break;
+        }else{
+            cout << "Please enter a valid number of players (integer, no greater than 4 and no less than 2)" << endl << ">>";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+
     DemonstratePlayers();
+  
+    DemonstrateBiddingFacility(numberOfPlayers);
+
     return 0;
 }
