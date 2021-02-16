0949c550ddfb4f3d5aecda6488386d5f048628d4
simonbingo <45861823+simonbingo@users.noreply.github.com>
2021-02-16 18:07:57 -0500

    Added implementation specifically for Ass1. of asked functions and cr… (#1)
    * Added implementation specifically for Ass1. of asked functions and created PlayerDriver where functions are used
    
    * fixed changes to PR
    
    * made All data members of user-defined class type must be of pointer type

---
+++ src/Player/PlayerDriver.cpp
@@ -0,0 +1,20 @@
+#include "Player.h"
+#include <memory>
+#include <sstream>
+
+//Main driver for Ass1. to create and run desired functions of the player class
+void DemonstratePlayers(){
+    std::unique_ptr<Player> playerSimon (new Player("Montreal"));
+    playerSimon->BuildCity();
+    playerSimon->DestroyArmy();
+    playerSimon->MoveArmies();
+    playerSimon->MoveOverLand();
+    playerSimon->PayCoin();
+    playerSimon->PlaceNewArmies();
+
+    std::unique_ptr<Player> copyPlayerConstructor(new Player(*playerSimon.get())); //copy constructor
+    *copyPlayerConstructor = *playerSimon;//assignment operator
+
+    std::istringstream istringstream("Another city");//creating stream of string
+    istringstream>>*copyPlayerConstructor;//stream insertion operation
+}
