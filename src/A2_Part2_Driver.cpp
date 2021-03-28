d3bea92c91a77ece046cb8e953047b488d7fff2b
houssen-kassir <57235418+houssen-kassir@users.noreply.github.com>
2021-03-28 18:50:38 -0400

    A2 part2(3) (#31)
    * 4Players=4DifferentDecks
    
    * finishing part 2 driver
    
    Co-authored-by: Houssen <houssen.kassir@hotmail.com>

--- src/A2_Part2_Driver.cpp
+++ src/A2_Part2_Driver.cpp
@@ -24,6 +24,12 @@ void DemonstrateA2Part2(){
                        3,
                        true));
     player1->hand_->SetOwningPlayer(player1);
+    player1->getBiddingFacility()->SetOwningPlayer(player1);
+
+    cout<<"Enter Player 1 Name: "<<endl;
+    string temp=" ";
+    cin>>temp;
+    player1->getBiddingFacility()->SetLastName(temp);
 
     Player *player2(
             new Player("Montreal",
@@ -40,6 +46,12 @@ void DemonstrateA2Part2(){
                        3,
                        true));
     player2->hand_->SetOwningPlayer(player2);
+    player2->getBiddingFacility()->SetOwningPlayer(player2);
+
+    cout<<"Enter Player 2 Name: "<<endl;
+    cin>>temp;
+    player2->getBiddingFacility()->SetLastName(temp);
+
 
     Player *players[2] = {player1, player2};
 
@@ -54,7 +66,12 @@ void DemonstrateA2Part2(){
     cout << "Cards in Face Up Pile:" << endl;
     deck->PrintCardsIn((deck->GetFaceUpCards()));
 
+    cout<<"Neutral Armies has Been Placed in PlaceHolder Territory."<<endl;
 
+    cout<<endl;
+    cout<<"/////////////////////PLAYER1/////////////////////"<<endl;
+    cout<<endl;
+    //PLAYER#1//////////////////////////////////////////////////////////////////////////////////////////////////////////
     cout<<player1->getName()<<" currently has "<<player1->GetArmiesTokens()<<" Armies."<<endl;
     cout<<"Assigning 18 armies to "<<player1->getName()<<endl;
     player1->SetArmiesTokens(18);
@@ -74,14 +91,98 @@ void DemonstrateA2Part2(){
 
     terr->getHasCity()[player1->getName()]=true;
     player1->PlaceNewArmies(4);
+    cout<<"----------------------------------"<<endl;
 
     cout<<player1->getName()<<" Currently has "<<player1->getMoney()<<" Coins."<<endl;
     cout<<"Assigning 14 Coins to "<<player1->getName()<<endl;
     player1->setMoney(14);
     cout<<player1->getName()<<" Currently has "<<player1->getMoney()<<" Coins."<<endl;
+    cout<<endl;
+    cout<<"/////////////////////PLAYER2/////////////////////"<<endl;
+    cout<<endl;
+    //PLAYER#2//////////////////////////////////////////////////////////////////////////////////////////////////////////
+    cout<<player2->getName()<<" currently has "<<player2->GetArmiesTokens()<<" Armies."<<endl;
+    cout<<"Assigning 18 armies to "<<player2->getName()<<endl;
+    player2->SetArmiesTokens(18);
+    cout<<player2->getName()<<" currently has "<<player2->GetArmiesTokens()<<" Armies."<<endl;
+    cout<<"----------------------------------"<<endl;
+
+    cout<<player2->getName()<<" currently has "<<player2->GetCitiesDisks()<<" Cities."<<endl;
+    cout<<"Assigning 3 disks to "<<player2->getName()<<endl;
+    player2->SetCitiesDisks(3);
+    cout<<player2->getName()<<" currently has "<<player2->GetCitiesDisks()<<" Cities."<<endl;
+    cout<<"----------------------------------"<<endl;
+
+    cout<<"Placing 4 armies in starting Territories."<<endl;
+
+    terr->InsertNewArmyPlayerMapping(player2->getName());
+    terr->InsertNewCityPlayerMapping(player2->getName());
+
+    terr->getHasCity()[player2->getName()]=true;
+    player2->PlaceNewArmies(4);
+    cout<<"----------------------------------"<<endl;
+
+    cout<<player2->getName()<<" Currently has "<<player2->getMoney()<<" Coins."<<endl;
+    cout<<"Assigning 14 Coins to "<<player2->getName()<<endl;
+    player2->setMoney(14);
+    cout<<player2->getName()<<" Currently has "<<player2->getMoney()<<" Coins."<<endl;
+
+    cout<<"/////////////////////////////////"<<endl;
+
+    // enter bids
+    for(int i = 0; i < 2; i++){
+        cout << "Bidding details for player " << players[i]->getName() << endl;
 
 
+        players[i]->getBiddingFacility()->EnterBid();
+
+        for(int j = 0; j < 100; j++){
+            cout << endl;
+        }
+    }
+
+    // reveal bids
+    for(int i = 0; i < 2; i++) {
+        players[i]->getBiddingFacility()->RevealBid();
+    }
+
+    // determine bid winner
+    Player * winningBidPlayer;
+    int highestBid = -1;
+    for(int i = 0; i < 2; i++) {
+        if(*players[i]->getBiddingFacility()->GetBidAmount() > highestBid){
+            highestBid = *players[i]->getBiddingFacility()->GetBidAmount();
+            winningBidPlayer = players[i];
+        }
+    }
+
+    for(int i = 0; i < 2; i++) {
+        if(*players[i]->getBiddingFacility()->GetBidAmount() == highestBid &&
+           players[i]->getName().compare(winningBidPlayer->getName()) < 0){
+            winningBidPlayer = players[i];
+        }
+    }
+
+    for(int i = 0; i < 2; i++) {
+        if(players[i] == winningBidPlayer){
+            players[i]->getBiddingFacility()->ResolveBid(true);
+        }else{
+            players[i]->getBiddingFacility()->ResolveBid(false);
+        }
+    }
+
+    delete winningBidPlayer;
+    winningBidPlayer = nullptr;
+    delete player1;
+    player1 = nullptr;
+    delete player2;
+    player2 = nullptr;
+    delete players[0];
+    players[0] = nullptr;
+    delete players[1];
+    players[1] = nullptr;
 
 
+return;
 
 }
