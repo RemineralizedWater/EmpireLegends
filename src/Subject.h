4aca520bb4cb059dd2f2f86a5f642eb9383c76cf
whuras <46137966+whuras@users.noreply.github.com>
2021-04-16 16:25:07 -0400

    Add Observer and Subject Classes (#45)
    * Add Observer and Subject Classes
    
    * branch slight refactor
    
    Co-authored-by: RemineralizedWater <31147432+RemineralizedWater@users.noreply.github.com>

---
+++ src/Subject.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Observer.h"
+#include <list>
+
+using namespace std;
+
+// forward reference due to circular references
+class Observer;
+
+class Subject{
+public:
+    Subject();
+    ~Subject();
+    virtual void Attach(Observer* o);
+    virtual void Detach(Observer* o);
+    virtual void Notify(); // to be included in the child's method to notify all those who are observing it
+
+private:
+    list<Observer *> *observers;
+};
